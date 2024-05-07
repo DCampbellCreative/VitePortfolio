f32ea168cb22a260a21e32919ad379c3965f1e74
dcampbellcreative <dougcampbell1990@gmail.com>
2024-05-07 17:06:01 -0400

    fix import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./components/Home/Home";
+import { Home } from "./components/home/Home";
 import { CaseStudyPage } from "./components/case-study/CaseStudyPage";
 import "./App.css";
 

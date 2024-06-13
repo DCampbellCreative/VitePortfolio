b5ba198174854d2632d93fd6ca19a3f53b884ee9
dcampbellcreative <dougcampbell1990@gmail.com>
2024-06-13 16:15:21 -0400

    remove projects

--- src/Data/PROJECTS.js
+++ src/Data/PROJECTS.js
@@ -1,41 +1,8 @@
 import reactdex from "/assets/reactdex-3.png";
 import dontbudge from "/assets/dontbudge2.png";
-import SuperHealth from "/assets/super-health.png";
-import ObservationTest from "/assets/observation-test-1.png";
 import LawlerLiving from "/assets/lawler-living.png";
 
 export const PROJECTS = [
-  {
-    title: "Super Health",
-    description:
-      "A full stack application with a backend created in Java, Spring Boot, and Postgres. Front end created using React. Project with full CRUD functionality tested in Postman. Allows users to add patients and data about their doctors visits. Created as the final project of the Create Opportunity Columbia September 2023 cohort.",
-    tools: [
-      "REACT",
-      "CSS MODULES",
-      "JAVA",
-      "SPRINGBOOT",
-      "POSTGRES",
-      "POSTMAN",
-    ],
-    links: {
-      Frontend:
-        "https://github.com/DCampbellCreative/Create-Opp-Health-Final-UI",
-      Backend:
-        "https://github.com/DCampbellCreative/Create-Opp-Health-Final-API",
-    },
-    imgUrl: SuperHealth,
-  },
-  {
-    title: "Observation Test",
-    description:
-      "A number selection game created as part of Create Opportunity Columbia curriculum. Users must select numbers in order from two data sets. If an incorrect number is selected the numbers in the current block are shuffled. Records users' time and accuracy and displays them once the game is completed.",
-    tools: ["REACT", "CSS MODULES"],
-    links: {
-      App: "https://dcampbellcreative.github.io/React-Observation-Test/",
-      Code: "https://github.com/DCampbellCreative/React-Observation-Test",
-    },
-    imgUrl: ObservationTest,
-  },
   {
     title: "Lawler Living",
     description:
